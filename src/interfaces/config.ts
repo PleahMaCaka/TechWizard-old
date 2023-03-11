1c7d42340a95fd2fd2ea401c49e89e43d71e8706
pleahmacaka <pleahmacaka@gmail.com>
2023-03-12 00:13:11 +0900

    declare env type

--- src/interfaces/config.ts
+++
@@ -1,6 +0,0 @@
-export interface config {
-    debug: boolean
-    ADMIN_ID: string
-    ADMIN_GUILD_ID: string
-    ADMIN_GUILD_CHANNEL_ID: string
-}
---
+++ src/interfaces/node.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+    // noinspection JSUnusedGlobalSymbols
+    interface ProcessEnv {
+        DEBUG: string
+        ADMIN_ID: string
+        ADMIN_GUILD_ID: string
+        ADMIN_GUILD_CHANNEL_ID: string
+    }
+}
